8b548e0f33692ed719a719e742cb1f409a9664a6
nauravlrie <nauravlrie@gmail.com>
2025-09-24 10:40:38 +0700

    push 2

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama punya Jesyca");
-        System.out.println("User jesyca menambahkan baris baru");
+        System.out.println("User jesyca menambahkan baris baru ");
         
     }
 }
