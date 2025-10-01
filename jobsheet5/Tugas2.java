31a7309c906271193df335452e6db9e13f2dfece
nauravlrie <nauravlrie@gmail.com>
2025-10-01 10:12:21 +0700

    Tugas 2

--- jobsheet5/Tugas2.java
+++ jobsheet5/Tugas2.java
@@ -1,5 +1,20 @@
 package jobsheet5;
 
+import java.util.Scanner;
+
 public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Masukkan angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+        sc.close();
+    }
     
 }
