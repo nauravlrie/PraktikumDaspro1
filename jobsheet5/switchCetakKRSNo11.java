20d2150462967c4aa49d07c23491f2258291337b
nauravlrie <nauravlrie@gmail.com>
2025-10-01 08:50:24 +0700

    Modifikasi Percobaan 2

--- jobsheet5/switchCetakKRSNo11.java
+++ jobsheet5/switchCetakKRSNo11.java
@@ -2,7 +2,7 @@ package jobsheet5;
 
 import java.util.Scanner;
 
-public class switchCetakKRSNoPresensi {
+public class switchCetakKRSNo11 {
    public static void main(String[] args) {
     
     Scanner sc =  new Scanner(System.in);
