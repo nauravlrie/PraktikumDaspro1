6f0dc59928930a36d263f22b22e1eb39e5a6043f
nauravlrie <nauravlrie@gmail.com>
2025-10-08 09:17:25 +0700

    jobsheet6

--- jobsheet5/ifCetakKRSNo11.java
+++ jobsheet5/ifCetakKRSNo11.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class ifCetakKRSNo11 {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Cetak KRS SIAKAD");
-        System.out.println("Apakah UKT sudah lunas? (true/false); ");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
         String pesan = uktLunas ? "Pembayaran UKT terverifikasi" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
         System.out.println(pesan);
@@ -17,34 +16,16 @@ public class ifCetakKRSNo11 {
             System.out.println("Masukkan semester saat ini: ");
             int semester = sc.nextInt();
 
-            switch (semester) {
-             case 1:
-                    System.out.println("KRS Semester 1 ditampilkan");
-                    break;
-                case 2:
-                    System.out.println("KRS Semester 2 ditampilkan");
-                    break;
-                case 3:
-                    System.out.println("KRS Semester 3 ditampilkan");
-                    break;
-                case 4:
-                    System.out.println("KRS Semester 4 ditampilkan");
-                    break;
-                case 5:
-                    System.out.println("KRS Semester 5 ditampilkan");
-                    break;
-                case 6:
-                    System.out.println("KRS Semester 6 ditampilkan");
-                    break;
-                case 7:
-                    System.out.println("KRS Semester 7 ditampilkan");
-                    break;
-                case 8:
-                    System.out.println("KRS Semester 8 ditampilkan");
-                    break;
-                default:
-                    System.out.println("Semester tidak valid");
-            }
+            String krsPesan = (semester == 1) ? "KRS Semester 1 ditampilkan" :
+                            (semester == 2) ? "KRS Semester 2 ditampilkan" :
+                            (semester == 3) ? "KRS Semester 3 ditampilkan" :
+                            (semester == 4) ? "KRS Semester 4 ditampilkan" :
+                            (semester == 5) ? "KRS Semester 5 ditampilkan" :
+                            (semester == 6) ? "KRS Semester 6 ditampilkan" :
+                            (semester == 7) ? "KRS Semester 7 ditampilkan" :
+                            (semester == 8) ? "KRS Semester 8 ditampilkan" : "Semester tidak valid";
+            System.out.println(krsPesan);
+
             System.out.println("Silahkan minta tanda tangan DPA");
         } else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
@@ -53,7 +34,3 @@ public class ifCetakKRSNo11 {
         sc.close();
     }
 }
-
-            
-        
-       
